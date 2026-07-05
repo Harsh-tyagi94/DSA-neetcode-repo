fc2fc8e47cea864712c22354812eb2c9ba6c1b8c
Harshtyagi94 <htyagi_be22@thapar.edu>
2026-07-05 22:11:25 +0530

    Add: delete-node-in-a-bst - submission-2

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-2.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deletionOP(TreeNode* root, int key) {
+        if(!root->left && !root->right) return nullptr;
+        else if(!root->left) return root->right;
+        else if(!root->right) return root->left;
+        else {
+            TreeNode* temp = root->right;
+            while(temp->left) temp = temp->left;
+            temp->left = root->left;
+            return root->right;
+        }
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root) return root;
+
+        if(root->val == key) return deletionOP(root, key);
+        else if(root->val > key) root->left = deleteNode(root->left, key);
+        else root->right = deleteNode(root->right, key);
+
+        return root;
+    }
+};
